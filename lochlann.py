c701ecd0fd02df2843c786a3c1e9a3ae825e197d
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-10-19 23:30:05 +0100

    register new user

--- lochlann.py
+++ lochlann.py
@@ -3,6 +3,19 @@ from reading_from_user import read_nonempty_string
 
 FILE_PASSWORDS = "passwords.txt"
 
+# Register a new user
+def register_user():
+    username = read_nonempty_string("Enter a new username: ")
+    password = read_nonempty_string("Enter a password: ")
+    access_key = read_nonempty_string("Enter your AWS Access Key ID: ")
+    secret_key = read_nonempty_string("Enter your AWS Secret Access Key: ")
+    
+    # Append new user to file
+    with open(FILE_PASSWORDS, "a") as file:
+        file.write(f"{username}\t{password}\t{access_key}\t{secret_key}\n")
+    
+    print(f"User '{username}' registered successfully.")
+
 # Main function to control the login and registration flow
 def main():
     while True:
