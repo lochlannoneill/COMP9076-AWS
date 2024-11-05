f7375c59ebb569992b0c5c93d20377e01ba27445
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-05 21:52:16 +0000

    input validation

--- EC2.py
+++ EC2.py
@@ -1,4 +1,5 @@
 import boto3
+from reading_from_user import read_nonempty_string
 
 class EC2Manager:
     def __init__(self, session):
@@ -36,25 +37,25 @@ class EC2Manager:
 
     def start_instance(self):
         """Start a specified EC2 instance."""
-        instance_id = input("Enter the Instance ID to start: ")
+        instance_id = read_nonempty_string("Enter the Instance ID to start: ")
         self.ec2.start_instances(InstanceIds=[instance_id])
         print(f"Starting instance {instance_id}...")
 
     def stop_instance(self):
         """Stop a specified EC2 instance."""
-        instance_id = input("Enter the Instance ID to stop: ")
+        instance_id = read_nonempty_string("Enter the Instance ID to stop: ")
         self.ec2.stop_instances(InstanceIds=[instance_id])
         print(f"Stopping instance {instance_id}...")
 
     def create_ami(self):
         """Create an AMI from a specified EC2 instance."""
-        instance_id = input("Enter the Instance ID to create AMI from: ")
-        ami_name = input("Enter a name for the AMI: ")
+        instance_id = read_nonempty_string("Enter the Instance ID to create AMI from: ")
+        ami_name = read_nonempty_string("Enter a name for the AMI: ")
         response = self.ec2.create_image(InstanceId=instance_id, Name=ami_name)
         print(f"AMI created: {response['ImageId']}")
 
     def delete_ami(self):
         """Delete a specified AMI."""
-        ami_id = input("Enter the AMI ID to delete: ")
+        ami_id = read_nonempty_string("Enter the AMI ID to delete: ")
         self.ec2.deregister_image(ImageId=ami_id)
         print(f"Deleted AMI: {ami_id}")
