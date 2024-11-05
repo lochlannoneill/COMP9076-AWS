4890d015151821879cfa9dfcdbb08270850b9fd0
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-05 21:50:03 +0000

    seperate entry-point logic into own files/classes

--- UserManager.py
+++ UserManager.py
@@ -1,7 +1,7 @@
+# User.py
 import os
-from reading_from_user import read_nonempty_string, read_range_integer
+from reading_from_user import read_nonempty_string
 
-FILE_PASSWORDS = "passwords.txt"
 FILE_FIELDS_COUNT = 4
 
 class UserManager:
@@ -59,46 +59,3 @@ class UserManager:
             "access_key": access_key,
             "secret_key": secret_key
         }
-
-class App:
-    MENU_OPTIONS = {
-        "Login": 1,
-        "Register": 2
-    }
-
-    def __init__(self, user_manager):
-        self.user_manager = user_manager
-
-    def display_menu(self):
-        """Display the main menu options to the user."""
-        print("Press 'ctrl+c' to exit.")
-        for option, number in self.MENU_OPTIONS.items():
-            print(f"{number}. {option}")
-
-    def handle_menu(self):
-        """Handle the user's menu selection."""
-        while True:
-            self.display_menu()
-            option = read_range_integer(
-                "Select from menu: ",
-                min(self.MENU_OPTIONS.values()),
-                max(self.MENU_OPTIONS.values())
-            )
-
-            if option == self.MENU_OPTIONS["Login"]:
-                user_credentials = self.user_manager.login_user()
-                if user_credentials:
-                    # Pass user_credentials to AWS handling functions if needed
-                    break  # Exit the menu after successful login
-            elif option == self.MENU_OPTIONS["Register"]:
-                self.user_manager.register_user()
-            else:
-                print("Invalid menu option.")
-
-def main():
-    user_manager = UserManager(FILE_PASSWORDS)
-    app = App(user_manager)
-    app.handle_menu()
-
-if __name__ == "__main__":
-    main()
