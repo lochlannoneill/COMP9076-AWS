c15292eaf4216554e6a8dcf6b370791356091a99
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-05 21:55:16 +0000

    input validation

--- Volumes.py
+++ Volumes.py
@@ -1,4 +1,5 @@
 import boto3
+from reading_from_user import read_nonnegative_integer, read_nonempty_string
 
 class Volumes:
     def __init__(self, session):
@@ -17,27 +18,27 @@ class Volumes:
 
     def create_volume(self):
         """Create a new EBS volume."""
-        size = int(input("Enter the size of the volume (GiB): "))
-        az = input("Enter the Availability Zone: ")
+        size = read_nonnegative_integer("Enter the size of the volume (GiB): ")
+        az = read_nonempty_string("Enter the Availability Zone: ")
         response = self.ec2.create_volume(Size=size, AvailabilityZone=az)
         print(f"Volume created: {response['VolumeId']}")
 
     def attach_volume(self):
         """Attach a volume to an EC2 instance."""
-        volume_id = input("Enter the Volume ID to attach: ")
-        instance_id = input("Enter the Instance ID to attach to: ")
-        device = input("Enter the device name (e.g., /dev/sdf): ")
+        volume_id = read_nonempty_string("Enter the Volume ID to attach: ")
+        instance_id = read_nonempty_string("Enter the Instance ID to attach to: ")
+        device = read_nonempty_string("Enter the device name (e.g., /dev/sdf): ")
         response = self.ec2.attach_volume(VolumeId=volume_id, InstanceId=instance_id, Device=device)
         print(f"Volume attached: {response['State']}")
 
     def detach_volume(self):
         """Detach a volume from an EC2 instance."""
-        volume_id = input("Enter the Volume ID to detach: ")
+        volume_id = read_nonempty_string("Enter the Volume ID to detach: ")
         response = self.ec2.detach_volume(VolumeId=volume_id)
         print(f"Volume detached: {response['State']}")
 
     def delete_volume(self):
         """Delete a volume."""
-        volume_id = input("Enter the Volume ID to delete: ")
+        volume_id = read_nonempty_string("Enter the Volume ID to delete: ")
         self.ec2.delete_volume(VolumeId=volume_id)
         print(f"Deleted volume: {volume_id}")
