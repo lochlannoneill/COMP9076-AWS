97d4f46b12070d7d24bf39ca2d455fb55168b909
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-06 01:20:08 +0000

    fix - ec2 menu display

--- src/menu/awsMenu.py
+++ src/menu/awsMenu.py
@@ -6,12 +6,14 @@ class awsMenu:
         print("1. EC2 Instances")
         print("2. Back to Main Menu")
 
-    def handle(self, ec2_service):
-        if choice == 1:
-            return True
-        elif choice == 2:
-            return False
-        else:
-            print("Invalid option.")
-            return False
+    def handle(self, ec2_service, ec2_menu):
+        while True:
+            self.display()
             choice = read_range_integer("Select from menu: ", 1, 2)
+            if choice == 1:
+                return ec2_menu.handle(ec2_service)
+            elif choice == 2:
+                return False
+            else:
+                print("Invalid option.")
+                return False
--- src/menu/ec2Menu.py
+++ src/menu/ec2Menu.py
@@ -11,20 +11,22 @@ class ec2Menu:
         print("6. Back")
 
     def handle(self, ec2_service):
+        while True:
+            self.display()
             choice = read_range_integer("Select from menu: ", 1, 6)
 
-        if choice == 1:
-            ec2_service.list_instances()
-        elif choice == 2:
-            ec2_service.start_instance()
-        elif choice == 3:
-            ec2_service.stop_instance()
-        elif choice == 4:
-            ec2_service.create_ami()
-        elif choice == 5:
-            ec2_service.delete_ami()
-        elif choice == 6:
-            return False
-        else:
-            print("Invalid option.")
-        return True
+            if choice == 1:
+                ec2_service.list_instances()
+            elif choice == 2:
+                ec2_service.start_instance()
+            elif choice == 3:
+                ec2_service.stop_instance()
+            elif choice == 4:
+                ec2_service.create_ami()
+            elif choice == 5:
+                ec2_service.delete_ami()
+            elif choice == 6:
+                return False
+            else:
+                print("Invalid option.")
+            return True
