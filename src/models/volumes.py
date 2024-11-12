285dca7b8bc5d5115836d9bd2671f083abd9b64d
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-12 20:56:37 +0000

    restore deleted code

--- src/models/volumes.py
+++ src/models/volumes.py
@@ -39,4 +39,29 @@ class Volumes:
         else:
             print("No unused volumes detected.")
 
-    # Other methods (create_volume, attach_volume, etc.) remain the same.
+    def create_volume(self):
+        """Create a new EBS volume."""
+        size = read_nonnegative_integer("Enter the size of the volume (GiB): ")
+        az = read_nonempty_string("Enter the Availability Zone: ")
+        response = self.ec2.create_volume(Size=size, AvailabilityZone=az)
+        print(f"Volume created: {response['VolumeId']}")
+
+    def attach_volume(self):
+        """Attach a volume to an EC2 instance."""
+        volume_id = read_nonempty_string("Enter the Volume ID to attach: ")
+        instance_id = read_nonempty_string("Enter the Instance ID to attach to: ")
+        device = read_nonempty_string("Enter the device name (e.g., /dev/sdf): ")
+        response = self.ec2.attach_volume(VolumeId=volume_id, InstanceId=instance_id, Device=device)
+        print(f"Volume attached: {response['State']}")
+
+    def detach_volume(self):
+        """Detach a volume from an EC2 instance."""
+        volume_id = read_nonempty_string("Enter the Volume ID to detach: ")
+        response = self.ec2.detach_volume(VolumeId=volume_id)
+        print(f"Volume detached: {response['State']}")
+
+    def delete_volume(self):
+        """Delete a volume."""
+        volume_id = read_nonempty_string("Enter the Volume ID to delete: ")
+        self.ec2.delete_volume(VolumeId=volume_id)
+        print(f"Deleted volume: {volume_id}")
