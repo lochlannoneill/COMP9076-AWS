631e33c80b499846c1ebd8270bb5f634dda10f1c
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-12 21:09:57 +0000

    completed - create volume

--- src/models/volumes.py
+++ src/models/volumes.py
@@ -12,7 +12,7 @@ class Volumes:
         available_volumes = []
 
         # Parse volumes
-        for volume in self.volume_resource.volumes.all():
+        for volume in self.volume_resource.volumes.all():  # This should use volume_resource
             volume_info = {
                 "Volume ID": volume.volume_id,
                 "Size": f"{volume.size} GiB",
@@ -41,27 +41,41 @@ class Volumes:
 
     def create_volume(self):
         """Create a new EBS volume."""
-        size = read_nonnegative_integer("Enter the size of the volume (GiB): ")
-        az = read_nonempty_string("Enter the Availability Zone: ")
-        response = self.ec2.create_volume(Size=size, AvailabilityZone=az)
-        print(f"Volume created: {response['VolumeId']}")
+        size = read_nonnegative_integer("\nEnter the size of the volume (GiB): ")
+        
+        # List available zones for the region dynamically
+        available_zones = [az['ZoneName'] for az in self.ec2_client.describe_availability_zones()['AvailabilityZones']]
+        
+        print("Available zones:")
+        for idx, az in enumerate(available_zones, start=1):
+            print(f"\t{idx}. {az}")
+        
+        # Get valid availability zone from user input
+        az_index = read_nonnegative_integer("Select the Availability Zone by number: ") - 1
+        if 0 <= az_index < len(available_zones):
+            az = available_zones[az_index]
+            response = self.ec2_client.create_volume(Size=size, AvailabilityZone=az)
+            print(f"Volume created: {response['VolumeId']}")
+        else:
+            print("Invalid zone selected.")
+
 
     def attach_volume(self):
         """Attach a volume to an EC2 instance."""
         volume_id = read_nonempty_string("Enter the Volume ID to attach: ")
         instance_id = read_nonempty_string("Enter the Instance ID to attach to: ")
         device = read_nonempty_string("Enter the device name (e.g., /dev/sdf): ")
-        response = self.ec2.attach_volume(VolumeId=volume_id, InstanceId=instance_id, Device=device)
+        response = self.ec2_client.attach_volume(VolumeId=volume_id, InstanceId=instance_id, Device=device)
         print(f"Volume attached: {response['State']}")
 
     def detach_volume(self):
         """Detach a volume from an EC2 instance."""
         volume_id = read_nonempty_string("Enter the Volume ID to detach: ")
-        response = self.ec2.detach_volume(VolumeId=volume_id)
+        response = self.ec2_client.detach_volume(VolumeId=volume_id)
         print(f"Volume detached: {response['State']}")
 
     def delete_volume(self):
         """Delete a volume."""
         volume_id = read_nonempty_string("Enter the Volume ID to delete: ")
-        self.ec2.delete_volume(VolumeId=volume_id)
+        self.ec2_client.delete_volume(VolumeId=volume_id)
         print(f"Deleted volume: {volume_id}")
