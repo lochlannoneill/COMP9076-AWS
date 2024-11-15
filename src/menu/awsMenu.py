6f0669b58f0631e7e47ac3d012f32fc7aac61c24
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-15 18:44:04 +0000

    resource creation after menu choice

--- src/menu/awsMenu.py
+++ src/menu/awsMenu.py
@@ -2,17 +2,17 @@ from src.utils.reading_from_user import read_range_integer
 from src.menu.ec2Menu import ec2Menu
 from src.menu.ebsMenu import ebsMenu
 from src.menu.s3Menu import s3Menu
+from src.models.resource import Resource
 from src.models.ebs import EBSController
 from src.models.ec2 import EC2Controller
 from src.models.s3 import S3Controller
 
 class awsMenu:
-    def __init__(self, resource):
-        self.ec2 = EC2Controller(resource.get_ec2_resource())
-        self.ebs = EBSController(resource.get_ec2_resource())
-        self.s3 = S3Controller(resource.get_s3_resource())
-        # self.cloudwatch_client = resource.get_cloudwatch_client()  # Example of using a client
-        
+    def __init__(self, user_credentials):
+        self.res = Resource(user_credentials)
+        self.ec2 = self.res.EC2Resource()
+        self.s3 = self.res.S3Resource()
+
         self.ec2_menu = ec2Menu()
         self.ebs_menu = ebsMenu()
         self.s3_menu = s3Menu()
@@ -37,15 +37,18 @@ class awsMenu:
             
             # EC2 Instances
             if choice == self.options["EC2 Instances"]:
-                self.ec2_menu.handle(self.ec2)
+                ec2_controller = EC2Controller(self.ec2)
+                self.ec2_menu.handle(ec2_controller)
                 
             # EBS Storage
             if choice == self.options["EBS Storage"]:
-                self.ebs_menu.handle(self.ebs)
+                ec2_controller = EBSController(self.ec2)
+                self.ebs_menu.handle(ec2_controller)
                 
             # S3 Storage
             if choice == self.options["S3 Storage"]:
-                self.s3_menu.handle(self.s3)
+                s3_controller = S3Controller(self.s3)
+                self.s3_menu.handle(s3_controller)
             
             # Monitoring
             if choice == self.options["Monitoring"]:
