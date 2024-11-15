64b0c9b2dff158dc1804c516c36dadfb4f594344
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-11-15 23:55:03 +0000

    remove redundant syntax duplication

--- src/app.py
+++ src/app.py
@@ -4,14 +4,11 @@ from src.menu.awsMenu import awsMenu
 from src.models.resource import Resource
 
 class App:
-    def __init__(self):
-        self.main_menu = mainMenu()
-    
-    def _start(self):
+    def main(self):
         """Main application loop handling authentication and AWS menu."""
         # Main menu loop
         while True:
-            user_credentials = self.main_menu.handle(UserManager())
+            user_credentials = mainMenu().handle(UserManager())
 
             # AWS menu loop
             if user_credentials:
@@ -19,9 +16,6 @@ class App:
                     awsMenu(user_credentials).handle()
                     break
 
-def main():
-    app = App()
-    app._start()
-
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.main()
